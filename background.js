f39c87dcfb05ae69bb6c0ff09154ea79aa0ee81c
happybigboy <mahdikhosravi83@outlook.com>
2024-11-28 17:33:35 +0330

    V2.4.0 Add new feature to check for new stars on GitHub repositories and notify users

--- background.js
+++ background.js
@@ -16,7 +16,42 @@ async function getUserData() {
     });
 }
 
+async function checkForNewStars() {
+    chrome.storage.local.get(['githubTokens', 'starredRepos'], async (result) => {
+        const tokens = result.githubTokens || {};
+        const previousStarredRepos = result.starredRepos || {};
+
+        for (const token of Object.values(tokens)) {
+            const response = await fetch('https://api.github.com/user/starred', {
+                headers: {
+                    'Authorization': `token ${token}`
+                }
+            });
+            const starredRepos = await response.json();
+
+            starredRepos.forEach(repo => {
+                if (!previousStarredRepos[repo.id]) {
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: 'icon.png',
+                        title: 'New Star!',
+                        message: `Your repository ${repo.name} has a new star!`
+                    });
+                }
+            });
+
+            const newStarredRepos = {};
+            starredRepos.forEach(repo => {
+                newStarredRepos[repo.id] = repo;
+            });
+
+            chrome.storage.local.set({ starredRepos: newStarredRepos });
+        }
+    });
+}
+
 getUserData();
+setInterval(checkForNewStars, 6000); // Check for new stars every minute
 chrome.action.onClicked.addListener(() => {
     chrome.tabs.create({ url: 'index.html' });
 });
